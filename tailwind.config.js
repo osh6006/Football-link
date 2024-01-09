66fb04d25475d687ab1eb2de2ceb2bf5c8ffa723
OHS <ohs6006@gmail.com>
2024-01-09 18:07:47 +0900

    Chore : tailwind color 세팅

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        Main: "#635FC7",
+        MainHover: "#A8A4FF",
+        DarkGrey: "#2B2C37",
+        MediumGrey: "#828FA3",
+        VeryDarkGreyDark: "#20212C",
+        LinesDark: "#3E3F4E",
+        LinesLight: "#E4EBFA",
+        LightGreyLightBg: "#F4F7FD",
+        Black: "#000112",
+        White: "#FFFFFF",
+        Red: "#EA5555",
+        RedHover: "#FF9898",
+      },
+    },
   },
   plugins: [],
 };
