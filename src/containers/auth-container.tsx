535a0a5fbf57adac9f37d422b161a54ff8c2379f
OHS <ohs6006@gmail.com>
2024-01-09 18:19:49 +0900

    Chore : tailwind prettier 설정

---
+++ src/containers/auth-container.tsx
@@ -0,0 +1,11 @@
+type Props = {};
+
+const AuthContainer = (props: Props) => {
+  return (
+    <main className="flex h-[100dvh] w-[100dvh] items-center justify-center">
+      AuthContainer
+    </main>
+  );
+};
+
+export default AuthContainer;
