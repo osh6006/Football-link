3a44af783d17b1d259ef281d85349109c60f0084
OHS <ohs6006@gmail.com>
2024-04-02 00:55:00 +0900

    Chore : 파일 네임 정리 & 쓸모 없는 파일 삭제

--- src/pages/before-login/auth-page.tsx
+++ src/pages/before-login/auth-page.tsx
@@ -1,13 +1,19 @@
-import { useAuthStepStore } from "../../stores/auth-step-store";
-
+import Button from "components/common/button";
 import AuthContainer from "../../components/layouts/auth-container";
-import StepOne from "../../components/auth/step-one";
-import StepThree from "../../components/auth/step-three";
-import StepTwo from "../../components/auth/step-two";
 import { LazyLoadComponent } from "react-lazy-load-image-component";
 
+import useAuth from "../../hooks/use-auth";
+
 const AuthPage = () => {
-  const { step } = useAuthStepStore();
+  const { signIn } = useAuth();
+
+  const handleGoogleBtn = async () => {
+    signIn("google");
+  };
+
+  const handleGithubBtn = async () => {
+    signIn("github");
+  };
 
   return (
     <AuthContainer>
@@ -17,12 +23,31 @@ const AuthPage = () => {
         sm:flex-row sm:justify-between"
         >
           <h1 className="sm:tex-xl text-lg">
-            Welcome to <strong className="text-Main">SpoLink !</strong>
+            Welcome to <strong className="text-Main">Football Link !</strong>
           </h1>
         </div>
-        {step === 1 && <StepOne />}
-        {step === 2 && <StepTwo />}
-        {step === 3 && <StepThree />}
+        <p className="mt-16 text-sm">
+          SNS <strong className="text-Main">Sign In / Sign Up</strong>
+        </p>
+        <div className="mt-2 flex w-1/2 flex-col gap-y-2">
+          <Button
+            onClick={handleGoogleBtn}
+            size="wide"
+            iconSrc="/svgs/google-icon.svg"
+            outline
+          >
+            Google
+          </Button>
+          <Button
+            onClick={handleGithubBtn}
+            size="wide"
+            iconSrc="/svgs/github-icon.svg"
+            outline
+          >
+            Github
+          </Button>
+        </div>
+
         <LazyLoadComponent>
           <div
             role="img"
