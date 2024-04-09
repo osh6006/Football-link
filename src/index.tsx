de5ff0716ce10ba62113ac892c037a4a1ee11327
OHS <ohs6006@gmail.com>
2024-04-09 15:28:31 +0900

    Fix : packge.json file error fix 2

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,19 @@
 import React from "react";
-import { hydrate, render } from "react-dom";
+import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 
 import RootProvider from "./providers/root-provider";
 
 import "./index.css";
 
-const rootElement = document.getElementById("root") as HTMLElement;
-const app = (
-  <React.StrictMode>
-    <RootProvider />
-  </React.StrictMode>
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement,
 );
-
-if (rootElement?.hasChildNodes()) {
-  hydrate(app, rootElement);
-} else {
-  render(app, rootElement);
-}
+root.render(
+  <React.StrictMode>
+    <RootProvider></RootProvider>
+  </React.StrictMode>,
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
