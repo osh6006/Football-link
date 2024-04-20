cc97ded78890e0ecd78e267e7ec45f1392d13f80
OHS <ohs6006@gmail.com>
2024-04-20 17:15:15 +0900

    Refactor : home news embla carousel 적용

--- src/components/home/home-news-table.tsx
+++ src/components/home/home-news-table.tsx
@@ -1,18 +1,21 @@
 import { componentBackgroundChange } from "utils/util";
 
+import Title from "./title";
 import HomeNewsCard from "./home-news-card";
-import { ArrowFix } from "components/common/util";
 import HomeNewsSkeleton from "./home-news-skeleton";
-import Carousel, { ResponsiveType } from "react-multi-carousel";
-import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
+import MoreArrow from "components/common/more-arrow";
 
 import { useTheme } from "stores/theme-store";
-import { useGlobalNewsQuery } from "hooks/services/quries/use-news-query";
+import useEmblaCarousel from "embla-carousel-react";
 import { useLeagueStore } from "stores/league-store";
+import {
+  NextButton,
+  PrevButton,
+  useNewsPrevNextButtons,
+} from "./news-carousel-buttons";
+import { useGlobalNewsQuery } from "hooks/services/quries/use-news-query";
 
-import "react-multi-carousel/lib/styles.css";
-import Title from "./title";
-import MoreArrow from "components/common/more-arrow";
+import styles from "./news.module.css";
 
 interface IHomeNewTableProps {}
 
@@ -24,41 +27,14 @@ const HomeNewsTable: React.FunctionComponent<IHomeNewTableProps> = () => {
     true,
   );
 
-  const responsive: ResponsiveType = {
-    desktop: {
-      breakpoint: { max: 3000, min: 1400 },
-      items: 3,
-      slidesToSlide: 3, // optional, default to 1.
-    },
+  const [emblaRef, emblaApi] = useEmblaCarousel({});
 
-    laptop: {
-      breakpoint: { max: 1400, min: 1024 },
-      items: 2,
-      slidesToSlide: 2, // optional, default to 1.
-    },
-
-    tablet: {
-      breakpoint: { max: 1024, min: 430 },
-      items: 2,
-      slidesToSlide: 2, // optional, default to 1.
-    },
-
-    mobile: {
-      breakpoint: { max: 430, min: 350 },
-      items: 1,
-      slidesToSlide: 1, // optional, default to 1.
-      partialVisibilityGutter: 1,
-      paritialVisibilityGutter: 1,
-    },
-
-    test: {
-      breakpoint: { max: 350, min: 0 },
-      items: 1,
-      slidesToSlide: 1, // optional, default to 1.
-      partialVisibilityGutter: 1,
-      paritialVisibilityGutter: 1,
-    },
-  };
+  const {
+    prevBtnDisabled,
+    nextBtnDisabled,
+    onPrevButtonClick,
+    onNextButtonClick,
+  } = useNewsPrevNextButtons(emblaApi);
 
   return (
     <div className="overflow-hidden">
@@ -79,57 +55,39 @@ const HomeNewsTable: React.FunctionComponent<IHomeNewTableProps> = () => {
           </div>
         ) : null}
         {isSuccess ? (
-          <Carousel
-            arrows
-            infinite
-            swipeable
-            draggable
-            rtl={false}
-            pauseOnHover
-            itemClass=""
-            sliderClass=""
-            rewind={false}
-            keyBoardControl
-            dotListClass=""
-            showDots={false}
-            slidesToSlide={1}
-            centerMode={false}
-            shouldResetAutoplay
-            autoPlaySpeed={3000}
-            focusOnSelect={false}
-            minimumTouchDrag={80}
-            additionalTransfrom={0}
-            responsive={responsive}
-            renderDotsOutside={false}
-            rewindWithAnimation={false}
-            renderArrowsWhenDisabled={false}
-            renderButtonGroupOutside={false}
-            containerClass="container-with-dots"
-            customLeftArrow={
-              <ArrowFix className="absolute left-4 cursor-pointer rounded-full bg-black/40 p-1.5 text-white transition-colors hover:bg-black/80">
-                <ChevronLeftIcon />
-              </ArrowFix>
-            }
-            customRightArrow={
-              <ArrowFix className="absolute right-4 cursor-pointer rounded-full bg-black/40 p-1.5 text-white transition-colors hover:bg-black/80">
-                <ChevronRightIcon />
-              </ArrowFix>
-            }
-          >
-            {data?.map((el, i) => {
-              return (
-                <HomeNewsCard
-                  author={el.author}
-                  imgUrl={el.urlToImage}
-                  title={el.title}
-                  url={el.url}
-                  desc={el.description}
-                  date={el.publishedAt}
-                  key={el.title + el.description}
-                />
-              );
-            })}
-          </Carousel>
+          <section className={styles.embla}>
+            <div className={styles.embla__viewport} ref={emblaRef}>
+              <div className={styles.embla__container}>
+                {data?.map((el, i) => {
+                  return (
+                    <div
+                      className={styles.embla__slide}
+                      key={el.title + el.description}
+                    >
+                      <HomeNewsCard
+                        author={el.author}
+                        imgUrl={el.urlToImage}
+                        title={el.title}
+                        url={el.url}
+                        desc={el.description}
+                        date={el.publishedAt}
+                      />
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+            <div className="absolute top-1/2 z-20 flex w-full justify-between px-10">
+              <PrevButton
+                onClick={onPrevButtonClick}
+                disabled={prevBtnDisabled}
+              />
+              <NextButton
+                onClick={onNextButtonClick}
+                disabled={nextBtnDisabled}
+              />
+            </div>
+          </section>
         ) : null}
       </div>
     </div>
