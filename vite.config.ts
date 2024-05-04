e423b1858b2ed687ddb953aea37dfba7528aad8f
OHS <ohs6006@gmail.com>
2024-05-04 09:23:54 +0900

    Feat : vite-plugin-pwa 설정 & vercel pwa 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,46 @@
-import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vitest/config";
 import tsconfigPaths from "vite-tsconfig-paths";
+
+const { VitePWA } = require("vite-plugin-pwa");
+
 import tailwindcss from "tailwindcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/",
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    VitePWA({
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: "Football Link",
+        short_name: "FLink",
+        description:
+          "Football Link is your ultimate destination for the latest football news, match highlights, scores, and analysis. Stay updated with all the action from the world of football with Football Link.",
+        icons: [
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+      },
+    }),
+  ],
   css: {
     postcss: {
       plugins: [tailwindcss()],
@@ -22,14 +56,6 @@ export default defineConfig({
       reporter: ["text", "json", "html"],
       include: ["src/**/*"],
       exclude: [],
-    },
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        main: "./index.html",
-        sw: "./sw.js",
-      },
     },
   },
 });
